6692d94681fa04c187a45ade73a3ffddc62c8c63
itsselms <92979021+itsselms@users.noreply.github.com>
2022-05-29 22:11:21 +0200

    Add files via upload

---
+++ storage/framework/views/2fce3637ca3f8aedd2593a50f6ecf1b0dc7e1abd.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH C:\Users\Selma\OneDrive - INTERNATIONAL UNIVERSITY OF SARAJEVO\Desktop\Porject Software Engeneering\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
